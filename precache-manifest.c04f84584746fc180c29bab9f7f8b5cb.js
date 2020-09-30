6fa9fe238357ae597da871ebca9871eab3fa16bd
reddpanes <alfred.panes@nutricoach.pro>
2020-09-30 23:20:18 +0800

    Updates

---
+++ precache-manifest.c04f84584746fc180c29bab9f7f8b5cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e03e4d284c1c5d07c72e3bcf7409cc",
+    "url": "/20200930-alfredpanes-trial/index.html"
+  },
+  {
+    "revision": "40d2f77196fdae61fc7a",
+    "url": "/20200930-alfredpanes-trial/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "1208648937101f989e14",
+    "url": "/20200930-alfredpanes-trial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "40d2f77196fdae61fc7a",
+    "url": "/20200930-alfredpanes-trial/static/js/2.b1432576.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/20200930-alfredpanes-trial/static/js/2.b1432576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1208648937101f989e14",
+    "url": "/20200930-alfredpanes-trial/static/js/main.b6fb9289.chunk.js"
+  },
+  {
+    "revision": "7b95a023bd5c597a64c8",
+    "url": "/20200930-alfredpanes-trial/static/js/runtime-main.8da88993.js"
+  }
+]);
