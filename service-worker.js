b85cfc5695cf897a52c4d8d02af24ba0ef56bf77
reddpanes <alfred.panes@nutricoach.pro>
2020-10-01 03:41:55 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/20200930-alfredpanes-trial/precache-manifest.c04f84584746fc180c29bab9f7f8b5cb.js"
+  "/20200930-alfredpanes-trial/precache-manifest.4e4656d97f0617b710e8865af2cb7c0d.js"
 );
 
 self.addEventListener('message', (event) => {
